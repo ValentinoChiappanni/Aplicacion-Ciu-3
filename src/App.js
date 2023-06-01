5d765cfe29f7cc8fb551022e40e0d997436168b1
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-06-01 19:51:38 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Mi primer app.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nueva app</h1>
     </div>
   );
 }
