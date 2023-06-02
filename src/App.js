3113b0c2d6637b5974f59a3a062e9fccceac0ecd
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-06-01 21:19:37 -0300

    se adelanta formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import React, { Fragment } from 'react';
+import { Formulario } from './components/Formulario';
 function App() {
   return (
-    <div className="App">
-      <h1>Nueva app</h1>
-    </div>
+    <Fragment>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Usuarios de la veterinaria</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {' '}
+            <Formulario />{' '}
+          </Col>
+          <Col>Listado de usuarios</Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,74 @@
+import React, { Fragment, useState, } from "react";
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+export function Formulario() {
+  // Creo un socio vacio y lo inicializo con un hook de estado
+  const [socio, editarSocio] = useState({
+    nombre: "",
+    dni: ""
+  })
+
+  // Estraer los valores
+  const { nombre, dni } = socio
+
+  // Creo otro hook de estado para manerar el error
+  const [error, setError] = useState(false)
+
+  // Recogemos lo que el usuario escribe en el formulario
+  const handleChange = (e) => {
+    editarSocio({
+      ...socio,
+      [e.target.name]: e.target.value
+    })
+    console.log("Tomo los datos...")
+  }
+
+  // 
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.log("Enviado")
+
+    if (nombre.trim() === '' || dni.trim() === '') {
+      setError(true)
+      return 1;
+    }
+
+    setError(false)
+  }
+
+  return (
+    <Fragment>
+      <Form onSubmit={submitForm}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Nombre completo"
+            name="nombre"
+            onChange={handleChange}
+            value={nombre} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>DNI</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Sin puntos ni espacios"
+            name="dni"
+            onChange={handleChange}
+            value={dni} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Ingresar socio
+        </Button>
+
+      </Form>
+
+      {
+        error ? <h4>Completa todos los campos</h4> : null
+      }
+    </Fragment>
+  )
+}
